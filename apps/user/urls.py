85d133243e473f48e8f4217cb8689c7f0f93f2d4
Leyton <1026158216@qq.com>
2020-02-28 12:21:46 +0800

    登录注册前端校验

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import RegisterView, ActiveView, LoginView,LogoutView, UserInfoView, UserOrderView, AddressView
+from .views import name_check, email_check, RegisterView, ActiveView, LoginView, LogoutView, UserInfoView, \
+    UserOrderView, AddressView
 
 app_name = 'user'
 urlpatterns = [
+    path('name_check/', name_check, name='name_check'),
+    path('email_check/', email_check, name='email_check'),
     path('register/', RegisterView.as_view(), name='register'),
     path('active/<str:token>', ActiveView.as_view(), name='active'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/',LogoutView.as_view(),name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('order/<int:page>', UserOrderView.as_view(), name='order'),
     path('address/', AddressView.as_view(), name='address'),
     path('', UserInfoView.as_view(), name='user'),
