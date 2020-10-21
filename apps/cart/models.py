2765780a7666724b73f036e24b2fecec0a406cf6
Leyton <1026158216@qq.com>
2020-10-21 10:30:42 +0800

    Revert "秋风"
    This reverts commit 619c6250c072df358d80711ad91b8227d78b90cf.
    
    fucku

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class FuckYou(models.Model):
-    name = models.CharField('名字', max_length=100)
-
-    class Meta:
-        verbose_name_plural = verbose_name = 'lm_fucku'
